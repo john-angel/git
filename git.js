ea8ed00575d5b8ff2e1799d1611e39f0555fca93
John Angel <john.angel@endava.com>
2021-08-06 10:25:29 -0500

    test: merge --squash add branch

--- git.js
+++ git.js
@@ -1,3 +1,15 @@
 function calc(){
-    
+
+    function sum(x,y){
+
+        if(typeof x === 'number' && typeof y === 'number'){
+            const result = x + y;
+            const description = `${x} + ${y} = ${result}`;
+            return {description, result};
+        }else{
+            throw(`Both input parameters have to be numbers: ${x}, ${y}`);
+        }
+        
+    }
+
 }
